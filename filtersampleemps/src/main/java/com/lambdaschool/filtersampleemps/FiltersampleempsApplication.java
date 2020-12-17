89dc8d24c8cc36b1fb990969844c701097d8b85c
chaz-kiker <chaz-kiker@lambdastudents.com>
2020-12-16 22:01:10 -0600

    create project

---
+++ filtersampleemps/src/main/java/com/lambdaschool/filtersampleemps/FiltersampleempsApplication.java
@@ -0,0 +1,15 @@
+package com.lambdaschool.filtersampleemps;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class FiltersampleempsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiltersampleempsApplication.class, args);
+	}
+
+}
